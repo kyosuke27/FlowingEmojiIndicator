5c1fd570ed0fc08e5cc2349bd4b895b6164d3b11
Kyosuke Noguchi <kn1359028527@gmail.com>
2026-06-11 00:12:02 +0900

    add デモ用のファイルを追加

---
+++ Example/DemoApp/DemoApp/DemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoAppApp.swift
+//  DemoApp
+//
+//  Created by kyosuke on 2026/06/10.
+//
+
+import SwiftUI
+
+@main
+struct DemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
