3133c8c88a4931c0852b904f51eb04d9439117fc
Kyosuke Noguchi <kn1359028527@gmail.com>
2026-06-12 01:42:41 +0900

    feat: 上下に揺れながら移動する

--- Example/DemoApp/DemoApp/ContentView.swift
+++ Example/DemoApp/DemoApp/ContentView.swift
@@ -6,14 +6,12 @@
 //
 
 import SwiftUI
+import FlowingEmojiIndicator
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            FlowingEmojiIndicator(emoji: "😊")
         }
         .padding()
     }
--- Sources/FlowingEmojiIndicator/FlowingEmojiIndicator.swift
+++ Sources/FlowingEmojiIndicator/FlowingEmojiIndicator.swift
@@ -1,11 +1,36 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
-
 import SwiftUI
 
-public struct FlowingEmojiIndicator:View{
-    public init() {}
+public struct FlowingEmojiIndicator: View {
+    private let horizontalPadding: CGFloat = 20
+    private let emojiWidth: CGFloat = 30
+    private let amplitude: CGFloat = 12
+    private let duration: TimeInterval = 3
+    let emoji:String
+
+    public init(emoji: String) {
+        self.emoji = emoji
+    }
+    
     public var body: some View {
-        Text("FlowingEmojiIndicator")
+        GeometryReader { geometry in
+            TimelineView(.animation) { timeline in
+                // 時間の変化量
+                let time = timeline.date.timeIntervalSinceReferenceDate
+                // 秒数の変化量を変化時間で割って、どの程度の進行具合かを割合に変換
+                let progress = (time.truncatingRemainder(dividingBy: duration)) / duration
+                
+                let startX = -emojiWidth - horizontalPadding
+                let endX = geometry.size.width + horizontalPadding
+                let x = startX + (endX - startX) * progress
+                
+                // sin波(3つの波を作る)
+                let y = sin(progress * 2 * .pi * 2) * amplitude
+                
+                Text(emoji)
+                    .frame(height:geometry.size.height)
+                    .offset(x: x, y: y)
+            }
+        }
+        .frame(height: 60)
     }
 }
