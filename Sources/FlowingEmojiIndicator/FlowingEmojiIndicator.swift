e243166ba8ad51c3d9d4cfd5eaf5850c78730a24
Kyosuke Noguchi <kn1359028527@gmail.com>
2026-06-11 23:49:33 +0900

    add パッケージの変更とライブラリの公開設定

--- Sources/FlowingEmojiIndicator/FlowingEmojiIndicator.swift
+++ Sources/FlowingEmojiIndicator/FlowingEmojiIndicator.swift
@@ -1,7 +1,11 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
+import SwiftUI
 
-public struct FlowingEmojiIndicator{
-    
+public struct FlowingEmojiIndicator:View{
+    public init() {}
+    public var body: some View {
+        Text("FlowingEmojiIndicator")
+    }
 }
