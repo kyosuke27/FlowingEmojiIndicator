2349fc1974cdc286191b4c503ca2b10f7e8adefd
Kyosuke Noguchi <kn1359028527@gmail.com>
2026-06-13 00:58:48 +0900

    refector: リファクタリング

--- Sources/FlowingEmojiIndicator/FlowingEmojiIndicator.swift
+++ Sources/FlowingEmojiIndicator/FlowingEmojiIndicator.swift
@@ -1,38 +1,20 @@
 import SwiftUI
 
 public struct FlowingEmojiIndicator: View {
-    private let horizontalPadding: CGFloat = 20
-    private let emojiWidth: CGFloat = 30
-    private let amplitude: CGFloat = 12
-    private let duration: TimeInterval = 3
+    let indicatorType:IndicatorType
     let emoji:String
-
-    public init(emoji: String) {
+    
+    public init(emoji: String,indicatorType:IndicatorType = .sin) {
         self.emoji = emoji
+        self.indicatorType = indicatorType
     }
     
     public var body: some View {
         GeometryReader { geometry in
-            // 描画レートの度に呼ばれる
-            TimelineView(.animation) { timeline in
-                // 時間の変化量
-                let time = timeline.date.timeIntervalSinceReferenceDate
-                // 秒数の変化量を変化時間で割って、どの程度の進行具合かを割合に変換
-                // truncatingReminderは少数で余りを求めるメソッド
-                let progress = (time.truncatingRemainder(dividingBy: duration)) / duration
-                
-                let startX = -emojiWidth - horizontalPadding
-                let endX = geometry.size.width + horizontalPadding
-                let x = startX + (endX - startX) * progress
-                
-                // sin波(3つの波を作る)
-                let y = sin(progress * 2 * .pi * 2) * amplitude
-                
-                Text(emoji)
-                    .frame(height:geometry.size.height)
-                    .offset(x: x, y: y)
+            switch self.indicatorType{
+                case .sin:
+                    SinWaveContent(emoji: self.emoji, geometry: geometry)
             }
         }
-        .frame(height: 60)
     }
 }
---
+++ Sources/FlowingEmojiIndicator/SinWaveContent.swift
@@ -0,0 +1,47 @@
+//
+//  File.swift
+//  FlowingEmojiIndicator
+//
+//  Created by kyosuke on 2026/06/12.
+//
+
+import SwiftUI
+
+struct SinWaveContent:View{
+    private let horizontalPadding: CGFloat = 20
+    private let emojiWidth: CGFloat = 30
+    private let amplitude: CGFloat = 12
+    private let duration: TimeInterval = 3
+    private let geometry:GeometryProxy
+    let emoji:String
+    
+    init(emoji: String,geometry:GeometryProxy) {
+        self.emoji = emoji
+        self.geometry = geometry
+    }
+    
+    var body: some View {
+        ZStack{
+            // 描画レートの度に呼ばれる
+            TimelineView(.animation) { timeline in
+                // 時間の変化量
+                let time = timeline.date.timeIntervalSinceReferenceDate
+                // 秒数の変化量を変化時間で割って、どの程度の進行具合かを割合に変換(0〜1の間)
+                // truncatingReminderは少数で余りを求めるメソッド
+                let progress = (time.truncatingRemainder(dividingBy: duration)) / duration
+                
+                let startX = -emojiWidth - horizontalPadding
+                let endX = geometry.size.width + horizontalPadding
+                let x = startX + (endX - startX) * progress
+                
+                // sin波(2つの波を作る)
+                let y = sin(progress * 2 * .pi * 2) * amplitude
+                HStack{
+                    Text("❤️")
+                        .offset(x: x, y: y)
+                }
+                
+            }
+        }
+    }
+}
